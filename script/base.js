df1c4962fb68ac6c096cbffe1c4153ca277ff07b
chanki <coldair426@gmail.com>
2023-01-26 19:43:13 +0900

    base.js 수정

--- script/base.js
+++ script/base.js
@@ -1,18 +1,10 @@
-const header = document.getElementsByClassName('header')[0]; // header get.
-const navigation = document.getElementsByClassName('navigation')[0];
-const mobileNavigationIcon = document.getElementsByClassName(
-  'header__mobile-navigation-icon'
-)[0];
-const mobileNavigationClose = document.getElementsByClassName(
-  'header__mobile-navigation-close'
-)[0];
-const mobileNavigationSliede = document.getElementsByClassName(
-  'header__mobile-navigation-slide'
-)[0];
-
 // sticky pc header.
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset > header.offsetHeight + navigation.offsetTop) {
+  if (
+    window.pageYOffset >
+    document.querySelector('.header').offsetHeight +
+      document.querySelector('.navigation').offsetTop
+  ) {
     document.querySelector('.sticky-header').style.visibility = 'visible';
     document.querySelector('.sticky-header').style.height = '60px';
   } else {
@@ -22,19 +14,36 @@ window.addEventListener('scroll', () => {
 });
 
 // 모바일 네비게이션 메뉴.
-mobileNavigationIcon.addEventListener('click', () => {
-  mobileNavigationSliede.style.visibility = 'visible';
-  mobileNavigationSliede.style.width = '60%';
-});
-mobileNavigationClose.addEventListener('click', () => {
-  mobileNavigationSliede.style.visibility = 'hidden';
-  mobileNavigationSliede.style.width = '0px';
-});
+document
+  .querySelector('.header__mobile-navigation-icon')
+  .addEventListener('click', () => {
+    document.querySelector(
+      '.header__mobile-navigation-slide'
+    ).style.visibility = 'visible';
+    document.querySelector('.header__mobile-navigation-slide').style.width =
+      '60%';
+  });
+document
+  .querySelector('.header__mobile-navigation-close')
+  .addEventListener('click', () => {
+    document.querySelector(
+      '.header__mobile-navigation-slide'
+    ).style.visibility = 'hidden';
+    document.querySelector('.header__mobile-navigation-slide').style.width =
+      '0px';
+  });
 // 모바일 네비게이션 메뉴가 펼쳐져 있을 때, 메인을 클릭하면 메뉴 닫힘.
 document.querySelector('main').addEventListener('click', () => {
-  if ((mobileNavigationSliede.style.visibility = 'visible')) {
-    mobileNavigationSliede.style.visibility = 'hidden';
-    mobileNavigationSliede.style.width = '0px';
+  if (
+    (document.querySelector(
+      '.header__mobile-navigation-slide'
+    ).style.visibility = 'visible')
+  ) {
+    document.querySelector(
+      '.header__mobile-navigation-slide'
+    ).style.visibility = 'hidden';
+    document.querySelector('.header__mobile-navigation-slide').style.width =
+      '0px';
   }
 });
 
